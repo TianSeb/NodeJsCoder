320cf4242f72649ed304b691674b3cabcd81dfb4
TianSeb <sebastianalasia86@gmail.com>
2023-07-06 23:28:09 -0300

    hot fix

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,4 +1,6 @@
 import { Router } from "express"
+import passport from "passport"
+import { isAuthenticated } from "../middlewares/AuthMw"
 import productsRoute from "./Product.routes"
 import cartsRoute from "./Cart.routes"
 import viewsRoutes from "./Views.routes"
