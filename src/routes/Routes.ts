b3e68407fe031789c9dc78fb2067013c73c45198
Sebastian Alasia <72724740+TianSeb@users.noreply.github.com>
2023-08-03 14:59:05 -0300

    mock endpoint

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -5,11 +5,12 @@ import productsRoute from "./Product.routes"
 import cartsRoute from "./Cart.routes"
 import viewsRoutes from "./Views.routes"
 import usersRoute from "./User.routes"
+import mockRoute from "./Mock.routes"
 
 const routes = Router()
 
 routes.use('/api', passport.authenticate('jwt'), isAuthenticated, productsRoute, cartsRoute)
 routes.use('/users', usersRoute)
-routes.use('/', viewsRoutes)
+routes.use('/', viewsRoutes, mockRoute)
 
 export default routes
