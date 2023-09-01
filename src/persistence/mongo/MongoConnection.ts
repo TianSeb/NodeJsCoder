09132e82d217cb3dcc8b1dcb3a7d17d5fc3957d4
TianSeb <sebastianalasia86@gmail.com>
2023-09-01 03:32:18 -0300

    git actions

--- src/persistence/mongo/MongoConnection.ts
+++ src/persistence/mongo/MongoConnection.ts
@@ -4,7 +4,8 @@ import createError from 'http-errors'
 import { logger } from '../../utils/Logger'
 
 export const initMongoDb = async () => {
-  mongoose.connect(config.mongoDatabaseUrl || "")
+  console.log(`URL IS HERE? ${config.mongoDatabaseUrl}`)
+  mongoose.connect(config.mongoDatabaseUrl)
     .then(() => {
       logger.debug('Mongo Db connection successful')
     }).catch(err => {
