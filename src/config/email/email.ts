a86400729294ee7dbd33d3bffe446ac0a5ccbccf
TianSeb <sebastianalasia86@gmail.com>
2023-09-01 01:58:42 -0300

    mailer fix

--- src/config/email/email.ts
+++ src/config/email/email.ts
@@ -11,48 +11,49 @@ dotenv.config({
   path: path.resolve(rootPath, `./.env.${process.env.NODE_ENV}`)
 })
 
+const mailConfig: any = {
+  host: process.env.EMAIL_HOST,
+  port: process.env.PORT_ETHEREAL,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+}
 
-// const transporter:Transporter = createTransport({
-//   host: process.env.EMAIL_HOST,
-//   port: process.env.PORT_ETHEREAL,
-//   auth: {
-//     user: process.env.EMAIL,
-//     pass: process.env.PASSWORD
-//   }
-// })
+const transporter: Transporter = createTransport(mailConfig)
 
-export async function sendOrderEmail(userEmail:any, totalPrice:any, products:any) {
+export async function sendOrderEmail(userEmail: any, totalPrice: any, products: any) {
   try {
-    // const templateFile = readFileSync(path.join(__dirname, 'email_template.ejs'), 'utf-8')
-    // const html = ejs.render(templateFile, { userEmail, totalPrice, products })
+    const templateFile = readFileSync(path.join(__dirname, 'email_template.ejs'), 'utf-8')
+    const html = ejs.render(templateFile, { userEmail, totalPrice, products })
 
-    // const mailOptions = {
-    //   from: process.env.EMAIL,
-    //   to: process.env.EMAIL,
-    //   subject: 'Purchase Order',
-    //   html: html,
-    // }
-    // await transporter.sendMail(mailOptions)
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: process.env.EMAIL,
+      subject: 'Purchase Order',
+      html: html,
+    }
+    await transporter.sendMail(mailOptions)
     logger.info('Email Sent:', userEmail)
   } catch (error) {
     logger.error('Error sending email:', error)
   }
 }
 
-export async function sendResetPassword(userEmail:any, hashCode:any) {
+export async function sendResetPassword(userEmail: any, hashCode: any) {
   try {
-    // const templateFile = readFileSync(path.join(__dirname, 'reset_password.ejs'), 'utf-8')
-    // const html = ejs.render(templateFile, { userEmail, hashCode })
+    const templateFile = readFileSync(path.join(__dirname, 'reset_password.ejs'), 'utf-8')
+    const html = ejs.render(templateFile, { userEmail, hashCode })
 
-    // const mailOptions = {
-    //   from: process.env.EMAIL,
-    //   to: process.env.EMAIL,
-    //   subject: 'Password Reset',
-    //   html: html,
-    // }
-    // await transporter.sendMail(mailOptions)
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: process.env.EMAIL,
+      subject: 'Password Reset',
+      html: html,
+    }
+    await transporter.sendMail(mailOptions)
     logger.info(`Email Sent: ${userEmail}`)
-  } catch (error:any) {
+  } catch (error: any) {
     logger.error(`Error sending email: ${error.msg}`)
   }
 }
