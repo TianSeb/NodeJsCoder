339d835e2a40c131e41f7f81b183245c60d74c4b
Sebastian Alasia <72724740+TianSeb@users.noreply.github.com>
2023-09-05 21:07:16 -0300

    entrega testing avanzado

--- src/persistence/mongo/MongoConnection.ts
+++ src/persistence/mongo/MongoConnection.ts
@@ -4,8 +4,6 @@ import createError from 'http-errors'
 import { logger } from '../../utils/Logger'
 
 export const initMongoDb = async () => {
-  console.log(`chequeando secret ${process.env.NODE_ENV}`)
-  console.log(`URL IS HERE? ${config.mongoDatabaseUrl}`)
   mongoose.connect(config.mongoDatabaseUrl)
     .then(() => {
       logger.debug('Mongo Db connection successful')
